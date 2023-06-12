4cf39c90625e0d4ad305fc2b9b89c3d8abbbbabe
Matt Helsley <matt.helsley+oss@gmail.com>
2023-06-11 18:07:07 -0700

    Fix Style breakage
    Move OSControl out of Style
    
    PR #43 created a regression where code that manually instantiated
    a Style could no longer be created without using the update
    operator and Default trait on Style.
    
    Rather than place non-public functions and data in Style move it
    all into AnsiGenericString. This has the added benefit of greatly
    simplifying the code.
    
    Also encloses the control sequences in \x01 .. \x02 to mark those
    portions which should be considered zero-width when displayed in
    the terminal.
    
    Fixes #46

--- src/display.rs
+++ src/display.rs
@@ -1,10 +1,35 @@
-use crate::ansi::{HYPERLINK_RESET, RESET};
+use crate::ansi::RESET;
 use crate::difference::Difference;
-use crate::style::{Color, OSControl, Style};
+use crate::style::{Color, Style};
 use crate::write::AnyWrite;
 use std::borrow::Cow;
 use std::fmt;
 use std::io;
+
+#[derive(Eq, PartialEq, Debug)]
+enum OSControl<'a, S: 'a + ToOwned + ?Sized>
+where
+    <S as ToOwned>::Owned: fmt::Debug,
+{
+    Title,
+    Icon,
+    Cwd,
+    Link { url: Cow<'a, S> },
+}
+
+impl<'a, S: 'a + ToOwned + ?Sized> Clone for OSControl<'a, S>
+where
+    <S as ToOwned>::Owned: fmt::Debug,
+{
+    fn clone(&self) -> Self {
+        match self {
+            Self::Link { url: u } => Self::Link { url: u.clone() },
+            Self::Title => Self::Title,
+            Self::Icon => Self::Icon,
+            Self::Cwd => Self::Cwd,
+        }
+    }
+}
 
 /// An `AnsiGenericString` includes a generic string type and a `Style` to
 /// display that string.  `AnsiString` and `AnsiByteString` are aliases for
@@ -16,7 +41,7 @@ where
 {
     pub(crate) style: Style,
     pub(crate) string: Cow<'a, S>,
-    pub(crate) params: Option<Cow<'a, S>>,
+    oscontrol: Option<OSControl<'a, S>>,
 }
 
 /// Cloning an `AnsiGenericString` will clone its underlying string.
@@ -38,7 +63,7 @@ where
         AnsiGenericString {
             style: self.style,
             string: self.string.clone(),
-            params: self.params.clone(),
+            oscontrol: self.oscontrol.clone(),
         }
     }
 }
@@ -100,7 +125,7 @@ where
         AnsiGenericString {
             string: input.into(),
             style: Style::default(),
-            params: None,
+            oscontrol: None,
         }
     }
 }
@@ -109,99 +134,6 @@ impl<'a, S: 'a + ToOwned + ?Sized> AnsiGenericString<'a, S>
 where
     <S as ToOwned>::Owned: fmt::Debug,
 {
-    /// Produce an ANSI string that changes the title shown
-    /// by the terminal emulator.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use nu_ansi_term::AnsiString;
-    /// let title_string = AnsiString::title("My Title");
-    /// println!("{}", title_string);
-    /// ```
-    /// Should produce an empty line but set the terminal title.
-    pub fn title<I>(title: I) -> AnsiGenericString<'a, S>
-    where
-        I: Into<Cow<'a, S>>,
-    {
-        Self {
-            string: title.into(),
-            style: Style::title(),
-            params: None,
-        }
-    }
-
-    /// Produce an ANSI string that notifies the terminal
-    /// emulator that the running application is better
-    /// represented by the icon found at a given path.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use nu_ansi_term::AnsiString;
-    /// let icon_string = AnsiString::icon(std::path::Path::new("foo/bar.icn").to_string_lossy());
-    /// println!("{}", icon_string);
-    /// ```
-    /// Should produce an empty line but set the terminal icon.
-    /// Notice that we use std::path to be portable.
-    pub fn icon<I>(path: I) -> AnsiGenericString<'a, S>
-    where
-        I: Into<Cow<'a, S>>,
-    {
-        Self {
-            string: path.into(),
-            style: Style::icon(),
-            params: None,
-        }
-    }
-
-    /// Produce an ANSI string that notifies the terminal
-    /// emulator the current working directory has changed
-    /// to the given path.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use nu_ansi_term::AnsiString;
-    /// let cwd_string = AnsiString::cwd(std::path::Path::new("/foo/bar").to_string_lossy());
-    /// println!("{}", cwd_string);
-    /// ```
-    /// Should produce an empty line but inform the terminal emulator
-    /// that the current directory is /foo/bar.
-    /// Notice that we use std::path to be portable.
-    pub fn cwd<I>(path: I) -> AnsiGenericString<'a, S>
-    where
-        I: Into<Cow<'a, S>>,
-    {
-        Self {
-            string: path.into(),
-            style: Style::cwd(),
-            params: None,
-        }
-    }
-
-    /// Cause the styled ANSI string to link to the given URL
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use nu_ansi_term::AnsiString;
-    /// use nu_ansi_term::Color::Red;
-    ///
-    /// let mut link_string = Red.paint("a red string");
-    /// link_string.hyperlink("https://www.example.com");
-    /// println!("{}", link_string);
-    /// ```
-    /// Should show a red-painted string which, on terminals
-    /// that support it, is a clickable hyperlink.
-    pub fn hyperlink<I>(&mut self, url: I)
-    where
-        I: Into<Cow<'a, S>>,
-    {
-        self.style.hyperlink();
-        self.params = Some(url.into());
-    }
-
     /// Directly access the style
     pub const fn style_ref(&self) -> &Style {
         &self.style
@@ -215,6 +147,115 @@ where
     // Directly access the underlying string
     pub fn as_str(&self) -> &S {
         self.string.as_ref()
+    }
+
+    // Instances that imply wrapping in OSC sequences
+    // and do not get displayed in the terminal text
+    // area.
+    //
+    /// Produce an ANSI string that changes the title shown
+    /// by the terminal emulator.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use nu_ansi_term::AnsiGenericString;
+    /// let title_string = AnsiGenericString::title("My Title");
+    /// println!("{}", title_string);
+    /// ```
+    /// Should produce an empty line but set the terminal title.
+    pub fn title<I>(s: I) -> Self
+    where
+        I: Into<Cow<'a, S>>,
+    {
+        Self {
+            style: Style::default(),
+            string: s.into(),
+            oscontrol: Some(OSControl::<'a, S>::Title),
+        }
+    }
+
+    /// Produce an ANSI string that notifies the terminal
+    /// emulator that the running application is better
+    /// represented by the icon found at a given path.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use nu_ansi_term::AnsiGenericString;
+    /// use std::path::Path;
+    /// let icon_string = AnsiGenericString::icon(Path::new("foo/bar.icn").to_string_lossy());
+    /// println!("{}", icon_string);
+    /// ```
+    /// Should produce an empty line but set the terminal icon.
+    /// Notice that we use std::path to be portable.
+    pub fn icon<I>(s: I) -> Self
+    where
+        I: Into<Cow<'a, S>>,
+    {
+        Self {
+            style: Style::default(),
+            string: s.into(),
+            oscontrol: Some(OSControl::<'a, S>::Icon),
+        }
+    }
+
+    /// Produce an ANSI string that notifies the terminal
+    /// emulator the current working directory has changed
+    /// to the given path.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use nu_ansi_term::AnsiGenericString;
+    /// use std::path::Path;
+    /// let cwd_string = AnsiGenericString::cwd(Path::new("/foo/bar").to_string_lossy());
+    /// println!("{}", cwd_string);
+    /// ```
+    /// Should produce an empty line but inform the terminal emulator
+    /// that the current directory is /foo/bar.
+    /// Notice that we use std::path to be portable.
+    pub fn cwd<I>(s: I) -> Self
+    where
+        I: Into<Cow<'a, S>>,
+    {
+        Self {
+            style: Style::default(),
+            string: s.into(),
+            oscontrol: Some(OSControl::<'a, S>::Cwd),
+        }
+    }
+
+    //
+    // Annotations (OSC sequences that do more than wrap)
+    //
+
+    /// Cause the styled ANSI string to link to the given URL
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use nu_ansi_term::Color::Red;
+    ///
+    /// let mut link_string = Red.paint("a red string");
+    /// link_string.hyperlink("https://www.example.com");
+    /// println!("{}", link_string);
+    /// ```
+    /// Should show a red-painted string which, on terminals
+    /// that support it, is a clickable hyperlink.
+    pub fn hyperlink<I>(&mut self, url: I)
+    where
+        I: Into<Cow<'a, S>>,
+    {
+        self.oscontrol = Some(OSControl::Link { url: url.into() });
+    }
+
+    /// Get any URL associated with the string
+    pub fn url_string(&self) -> Option<&S> {
+        match &self.oscontrol {
+            Some(OSControl::Link { url: u }) => Some(u.as_ref()),
+            _ => None,
+        }
     }
 }
 
@@ -259,7 +300,7 @@ impl Style {
         AnsiGenericString {
             string: input.into(),
             style: self,
-            params: None,
+            oscontrol: None,
         }
     }
 }
@@ -282,7 +323,7 @@ impl Color {
         AnsiGenericString {
             string: input.into(),
             style: self.normal(),
-            params: None,
+            oscontrol: None,
         }
     }
 }
@@ -310,13 +351,39 @@ where
     <S as ToOwned>::Owned: fmt::Debug,
     &'a S: AsRef<[u8]>,
 {
+    // write the part within the styling prefix and suffix
+    fn write_inner<W: AnyWrite<Wstr = S> + ?Sized>(&self, w: &mut W) -> Result<(), W::Error> {
+        // \x01 .. \x02 are used to wrap zero-width portions of output
+        match &self.oscontrol {
+            Some(OSControl::Link { url: u }) => {
+                write!(w, "\x01\x1B]8;;")?;
+                w.write_str(u.as_ref())?;
+                write!(w, "\x1B\x5C\x02")?;
+                w.write_str(self.string.as_ref())?;
+                write!(w, "\x01\x1B]8;;\x1B\x5C\x02")
+            }
+            Some(OSControl::Title) => {
+                write!(w, "\x01\x1B]2;")?;
+                w.write_str(self.string.as_ref())?;
+                write!(w, "\x1B\x5C\x02")
+            }
+            Some(OSControl::Icon) => {
+                write!(w, "\x01\x1B]I;")?;
+                w.write_str(self.string.as_ref())?;
+                write!(w, "\x1B\x5C\x02")
+            }
+            Some(OSControl::Cwd) => {
+                write!(w, "\x01\x1B]7;")?;
+                w.write_str(self.string.as_ref())?;
+                write!(w, "\x1B\x5C\x02")
+            }
+            None => w.write_str(self.string.as_ref()),
+        }
+    }
+
     fn write_to_any<W: AnyWrite<Wstr = S> + ?Sized>(&self, w: &mut W) -> Result<(), W::Error> {
         write!(w, "{}", self.style.prefix())?;
-        if let (Some(s), Some(_)) = (&self.params, self.style.oscontrol) {
-            w.write_str(s.as_ref())?;
-            write!(w, "\x1B\\")?;
-        }
-        w.write_str(self.string.as_ref())?;
+        self.write_inner(w)?;
         write!(w, "{}", self.style.suffix())
     }
 }
@@ -354,59 +421,16 @@ where
         };
 
         write!(w, "{}", first.style.prefix())?;
-        if let (Some(s), Some(_)) = (&first.params, first.style.oscontrol) {
-            w.write_str(s.as_ref())?;
-            write!(w, "\x1B\\")?;
-        }
-        w.write_str(first.string.as_ref())?;
+        first.write_inner(w)?;
 
         for window in self.0.windows(2) {
             match Difference::between(&window[0].style, &window[1].style) {
-                ExtraStyles(style) => {
-                    write!(w, "{}", style.prefix())?;
-                    if let (Some(OSControl::Hyperlink), Some(s)) =
-                        (style.oscontrol, &window[1].params)
-                    {
-                        w.write_str(s.as_ref())?;
-                        write!(w, "\x1B\\")?;
-                    }
-                }
-                Reset => match (&window[0].style, &window[1].style) {
-                    (
-                        Style {
-                            oscontrol: Some(OSControl::Hyperlink),
-                            ..
-                        },
-                        Style {
-                            oscontrol: None, ..
-                        },
-                    ) => {
-                        write!(
-                            w,
-                            "{}{}{}",
-                            HYPERLINK_RESET,
-                            RESET,
-                            window[1].style.prefix()
-                        )?;
-                    }
-                    (
-                        Style {
-                            oscontrol: Some(_), ..
-                        },
-                        Style {
-                            oscontrol: None, ..
-                        },
-                    ) => {
-                        write!(w, "\x1B\\{}", window[1].style.prefix())?;
-                    }
-                    (_, _) => {
-                        write!(w, "{}{}", RESET, window[1].style.prefix())?;
-                    }
-                },
+                ExtraStyles(style) => write!(w, "{}", style.prefix())?,
+                Reset => write!(w, "{}{}", RESET, window[1].style.prefix())?,
                 Empty => { /* Do nothing! */ }
             }
 
-            w.write_str(&window[1].string)?;
+            window[1].write_inner(w)?;
         }
 
         // Write the final reset string after all of the AnsiStrings have been
@@ -414,17 +438,7 @@ where
         // have already been written by this point.
         if let Some(last) = self.0.last() {
             if !last.style.is_plain() {
-                match last.style.oscontrol {
-                    Some(OSControl::Hyperlink) => {
-                        write!(w, "{}{}", HYPERLINK_RESET, RESET)?;
-                    }
-                    Some(_) => {
-                        write!(w, "\x1B\\")?;
-                    }
-                    _ => {
-                        write!(w, "{}", RESET)?;
-                    }
-                }
+                write!(w, "{}", RESET)?;
             }
         }
 
@@ -506,22 +520,22 @@ mod tests {
 
     #[test]
     fn title() {
-        let title = Style::title().paint("Test Title");
-        assert_eq!(title.clone().to_string(), "\x1B]2;Test Title\x1B\\");
+        let title = AnsiGenericString::title("Test Title");
+        assert_eq!(title.clone().to_string(), "\x01\x1B]2;Test Title\x1B\\\x02");
         idempotent(title)
     }
 
     #[test]
     fn icon() {
-        let icon = Style::icon().paint("/path/to/test.icn");
-        assert_eq!(icon.to_string(), "\x1B]I;/path/to/test.icn\x1B\\");
+        let icon = AnsiGenericString::icon("/path/to/test.icn");
+        assert_eq!(icon.to_string(), "\x01\x1B]I;/path/to/test.icn\x1B\\\x02");
         idempotent(icon)
     }
 
     #[test]
     fn cwd() {
-        let cwd = Style::cwd().paint("/path/to/test/");
-        assert_eq!(cwd.to_string(), "\x1B]7;/path/to/test/\x1B\\");
+        let cwd = AnsiGenericString::cwd("/path/to/test/");
+        assert_eq!(cwd.to_string(), "\x01\x1B]7;/path/to/test/\x1B\\\x02");
         idempotent(cwd)
     }
 
@@ -531,7 +545,7 @@ mod tests {
         styled.hyperlink("https://example.com");
         assert_eq!(
             styled.to_string(),
-            "\x1B[31m\x1B]8;;https://example.com\x1B\\Link to example.com.\x1B]8;;\x1B\\\x1B[0m"
+            "\x1B[31m\x01\x1B]8;;https://example.com\x1B\\\x02Link to example.com.\x01\x1B]8;;\x1B\\\x02\x1B[0m"
         );
     }
 
@@ -545,29 +559,29 @@ mod tests {
         // Assemble with link by itself
         let joined = AnsiStrings(&[link.clone()]).to_string();
         #[cfg(feature = "gnu_legacy")]
-        assert_eq!(joined, format!("\x1B[04;34m\x1B]8;;https://example.com\x1B\\Link to example.com.\x1B]8;;\x1B\\\x1B[0m"));
+        assert_eq!(joined, format!("\x1B[04;34m\x01\x1B]8;;https://example.com\x1B\\\x02Link to example.com.\x01\x1B]8;;\x1B\\\x02\x1B[0m"));
         #[cfg(not(feature = "gnu_legacy"))]
-        assert_eq!(joined, format!("\x1B[4;34m\x1B]8;;https://example.com\x1B\\Link to example.com.\x1B]8;;\x1B\\\x1B[0m"));
+        assert_eq!(joined, format!("\x1B[4;34m\x01\x1B]8;;https://example.com\x1B\\\x02Link to example.com.\x01\x1B]8;;\x1B\\\x02\x1B[0m"));
 
         // Assemble with link in the middle
         let joined = AnsiStrings(&[before.clone(), link.clone(), after.clone()]).to_string();
         #[cfg(feature = "gnu_legacy")]
-        assert_eq!(joined, format!("\x1B[32mBefore link. \x1B[04;34m\x1B]8;;https://example.com\x1B\\Link to example.com.\x1B]8;;\x1B\\\x1B[0m\x1B[32m After link.\x1B[0m"));
+        assert_eq!(joined, format!("\x1B[32mBefore link. \x1B[04;34m\x01\x1B]8;;https://example.com\x1B\\\x02Link to example.com.\x01\x1B]8;;\x1B\\\x02\x1B[0m\x1B[32m After link.\x1B[0m"));
         #[cfg(not(feature = "gnu_legacy"))]
-        assert_eq!(joined, format!("\x1B[32mBefore link. \x1B[4;34m\x1B]8;;https://example.com\x1B\\Link to example.com.\x1B]8;;\x1B\\\x1B[0m\x1B[32m After link.\x1B[0m"));
+        assert_eq!(joined, format!("\x1B[32mBefore link. \x1B[4;34m\x01\x1B]8;;https://example.com\x1B\\\x02Link to example.com.\x01\x1B]8;;\x1B\\\x02\x1B[0m\x1B[32m After link.\x1B[0m"));
 
         // Assemble with link first
         let joined = AnsiStrings(&[link.clone(), after.clone()]).to_string();
         #[cfg(feature = "gnu_legacy")]
-        assert_eq!(joined, format!("\x1B[04;34m\x1B]8;;https://example.com\x1B\\Link to example.com.\x1B]8;;\x1B\\\x1B[0m\x1B[32m After link.\x1B[0m"));
+        assert_eq!(joined, format!("\x1B[04;34m\x01\x1B]8;;https://example.com\x1B\\\x02Link to example.com.\x01\x1B]8;;\x1B\\\x02\x1B[0m\x1B[32m After link.\x1B[0m"));
         #[cfg(not(feature = "gnu_legacy"))]
-        assert_eq!(joined, format!("\x1B[4;34m\x1B]8;;https://example.com\x1B\\Link to example.com.\x1B]8;;\x1B\\\x1B[0m\x1B[32m After link.\x1B[0m"));
+        assert_eq!(joined, format!("\x1B[4;34m\x01\x1B]8;;https://example.com\x1B\\\x02Link to example.com.\x01\x1B]8;;\x1B\\\x02\x1B[0m\x1B[32m After link.\x1B[0m"));
 
         // Assemble with link at the end
         let joined = AnsiStrings(&[before.clone(), link.clone()]).to_string();
         #[cfg(feature = "gnu_legacy")]
-        assert_eq!(joined, format!("\x1B[32mBefore link. \x1B[04;34m\x1B]8;;https://example.com\x1B\\Link to example.com.\x1B]8;;\x1B\\\x1B[0m"));
+        assert_eq!(joined, format!("\x1B[32mBefore link. \x1B[04;34m\x01\x1B]8;;https://example.com\x1B\\\x02Link to example.com.\x01\x1B]8;;\x1B\\\x02\x1B[0m"));
         #[cfg(not(feature = "gnu_legacy"))]
-        assert_eq!(joined, format!("\x1B[32mBefore link. \x1B[4;34m\x1B]8;;https://example.com\x1B\\Link to example.com.\x1B]8;;\x1B\\\x1B[0m"));
+        assert_eq!(joined, format!("\x1B[32mBefore link. \x1B[4;34m\x01\x1B]8;;https://example.com\x1B\\\x02Link to example.com.\x01\x1B]8;;\x1B\\\x02\x1B[0m"));
     }
 }
